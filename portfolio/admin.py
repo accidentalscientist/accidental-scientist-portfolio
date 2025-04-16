335f5ddf0519f7e3d00c9933124e1b42ae51a49d
accidentalscientist <t.aymonier.newman@gmail.com>
2025-04-16 10:39:35 +1000

    Add initial migration files and templates for blog and project features

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project, BlogPost
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(BlogPost)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    ]
+    path('projects/', views.all_projects, name='projects'),
+    path('blog/', views.blog_list, name='blog'),
+    path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
+]
